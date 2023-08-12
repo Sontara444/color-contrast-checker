e9c5c607bdf998aa73a38a090de9d1c881b5db2d
Sontara444 <sontararajput@gmail.com>
2023-08-12 23:16:22 +0530

    css completed

---
+++ script.js
@@ -0,0 +1,5 @@
+let textColor = document.getElementById("text-color");
+let bgColor = document.getElementById("bg-color");
+let previewText = document.getElementById("preview");
+let contrast = document.getElementById("contrast");
+let rating = document.getElementById("rating");
